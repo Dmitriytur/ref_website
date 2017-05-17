2859445acd262e147e25a1d792120d63f35ca1a9
Dmitry Tur <dmitriy.tur.v@gmail.com>
2017-05-17 08:31:03 +0300

    profile add

--- src/main/java/ua/nure/tur/controllers/AccountController.java
+++ src/main/java/ua/nure/tur/controllers/AccountController.java
@@ -1,18 +1,22 @@
 package ua.nure.tur.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import ua.nure.tur.config.ConfigURL;
+import ua.nure.tur.entity.User;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 @Controller
@@ -49,5 +53,21 @@ public class AccountController {
         session.setAttribute("access_token", message.getBody());
     }
 
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public String profile(Model model, HttpSession session) throws UnirestException, IOException {
+        HttpResponse<String> response = Unirest.get(ConfigURL.getApiURL() + "/getProfile")
+                .header("content-type", "application/json")
+                .header("Authorization", session.getAttribute("access_token").toString())
+                .asString();
+        User user =  new ObjectMapper().readValue(response.getBody(), User.class);
+        model.addAttribute("user", user);
+        return "profile";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public void logout(HttpServletResponse response, HttpSession session) throws IOException {
+        session.removeAttribute("access_token");
+        response.sendRedirect("/home");
+    }
 
 }
--- src/main/java/ua/nure/tur/security/TokenAuthenticationService.java
+++ src/main/java/ua/nure/tur/security/TokenAuthenticationService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Date;
